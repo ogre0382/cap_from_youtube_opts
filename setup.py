21c6c27a010375f208c50f5f41300a80a0baf6e4
ogre0382 <115123784+ogre0382@users.noreply.github.com>
2023-10-13 23:16:04 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,14 +6,14 @@ this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 
 setup(
-    name='cap_from_youtube',
-    version='0.0.9',
+    name='cap_from_youtube_opts',
+    version='0.0.10.1',
     license='MIT',
-    description='Get an OpenCV video capture from an YouTube video URL',
+    description='Get an OpenCV video capture from an YouTube video URL + ydl_opts',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    author='Ibai Gorordo',
-    url='https://github.com/ibaiGorordo/cap_from_youtube',
+    author='ogre0382',
+    url='https://github.com/ogre0382/cap_from_youtube_opts',
     packages=find_packages(),
     install_requires=[
         'numpy',
